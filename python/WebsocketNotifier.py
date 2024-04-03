92237815b210976834150a2a047d809f45bf28dc
TobiasBak <tobib21@student.sdu.dk>
2024-04-03 14:41:25 +0200

    Documentation for websocket_notifier class

--- python/WebsocketNotifier.py
+++ python/WebsocketNotifier.py
@@ -2,13 +2,20 @@ from typing import Callable
 
 
 class WebsocketNotifier:
+    """
+    This class can be used to notify all registered observers with a message.
+
+    This is used to send messages to all the clients connected to the websocket server.
+    """
     def __init__(self):
         self._observers = []
 
     def register_observer(self, observer: Callable[[str], None]):
+        """ Register a function to be called when notify_observers is called"""
         self._observers.append(observer)
 
     def notify_observers(self, message: str):
+        """ Call all registered functions with the given message."""
         for observer in self._observers:
             observer(message)
 
