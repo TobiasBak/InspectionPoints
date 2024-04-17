c0d6c5e17cdac36f128683540b7e8d4d47f358d0
Kevin Torp <Kevintorp@hotmail.com>
2024-04-17 09:28:00 +0200

    Yeeted

--- python/RobotControl/RobotSocketVariableTypes.py
+++ python/RobotControl/RobotSocketVariableTypes.py
@@ -6,7 +6,6 @@ class VariableTypes(Enum):
     This is a list of variable types that can be sent to the robot
     Each type has a corresponding character that is used to identify the type
     """
-    STRING = None
     String = "<"
     Integer = "|"
     Float = "@"
