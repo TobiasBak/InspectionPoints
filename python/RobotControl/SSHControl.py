f46ba721789788a9ad8d38f3c12c58122d69a1a0
TobiasBak <tobib21@student.sdu.dk>
2025-04-11 11:33:55 +0200

    Scripts for loading and playing urp programs

--- python/RobotControl/SSHControl.py
+++ python/RobotControl/SSHControl.py
@@ -1,5 +1,11 @@
+import socket
+from socket import socket as Socket
+from time import sleep
+from typing import Callable
+import select
 
 
+from constants import ROBOT_IP, DASHBOARD_PORT
 
 def create_get_socket_function() -> Callable[[str, int], Socket]:
     inner_socket_bank: dict[tuple[str, int], Socket] = dict()
@@ -9,16 +15,11 @@ def create_get_socket_function() -> Callable[[str, int], Socket]:
             return inner_socket_bank[(ip, port)]
         try:
             my_socket: Socket = Socket(socket.AF_INET, socket.SOCK_STREAM)
-            non_recurring_logger.debug(f"Socket successfully created for {ip}:{port}")
         except socket.error as err:
-            non_recurring_logger.error(f"socket creation failed with error {err}")
             return
-
         try:
             my_socket.connect((ip, port))
-            non_recurring_logger.debug(f"Socket connected to {ip}:{port}")
         except ConnectionRefusedError:
-            non_recurring_logger.info(f"Connection to {ip}:{port} refused - retrying in 1 second")
             my_socket.close()
             sleep(1)
             return get_socket(ip, port)
@@ -29,12 +30,38 @@ def create_get_socket_function() -> Callable[[str, int], Socket]:
 
     return inner_get_socket
 
-
 get_socket = create_get_socket_function()
 
 def _get_dashboard_socket():
     return get_socket(ROBOT_IP, DASHBOARD_PORT)
 
+def read_from_socket(socket: Socket) -> str:
+    ready_to_read, ready_to_write, in_error = select.select([socket], [], [], 0.1)
+    if ready_to_read:
+        message = socket.recv(4096)
+
+        try:
+            return message.decode()
+        except UnicodeDecodeError as e:
+            # Intentionally not returning anything.
+            # Returning nothing if a decode error occurs.
+            print(f"Decode error: {e}")
+
+    return "nothing"
+
+
+def read_from_socket_till_end(socket: Socket) -> str:
+    out = ""
+    message = read_from_socket(socket)
+    while message != "nothing":
+        out += message
+        message = read_from_socket(socket)
+    return out
+
+def sanitize_command(command: str) -> str:
+    command = command.replace('\n', ' ')
+    command += "\n"  # Add a trailing \n character
+    return command
 
 def send_command_dashboard_socket(command: str) -> str:
     dashboard_socket = _get_dashboard_socket()
@@ -42,8 +69,13 @@ def send_command_dashboard_socket(command: str) -> str:
     dashboard_socket.send(sanitized_command.encode())
     return read_from_socket(dashboard_socket)
 
-# TODO Load urp program througrh dashboard server
-# TODO Play program through dashboard server  
+def load_urp_program(program_name: str):
+    assert(program_name.endswith(".urp"), "Program name must end with .urp")
+    send_command_dashboard_socket(f"load {program_name}")
+
+def start_loaded_program():
+    send_command_dashboard_socket("play")
+
 
 # TODO SSH and write urp file to server (maybe we can juste write to the urprograms)
 
