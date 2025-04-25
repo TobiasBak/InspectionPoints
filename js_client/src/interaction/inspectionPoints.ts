273f8ac92c5e96f18db45d9c1c8e754381eb52a8
Kevin Torp <kevintorp@hotmail.com>
2025-04-25 14:17:43 +0200

    Created decorations for monaco
    This is done with saving the lines pressed on as I think we need to track the pressed lines for backend inspection points

--- js_client/src/interaction/inspectionPoints.ts
+++ js_client/src/interaction/inspectionPoints.ts
@@ -1,3 +1,75 @@
-import { editor } from "../monacoExperiment";
+import * as monaco from 'monaco-editor';
+import { editorReady } from "../monacoExperiment";
 
+/**
+ * Updates the decorations in the editor.
+ * @param editor The Monaco editor instance.
+ * @param lineCount The total number of lines in the editor.
+ * @param clickedLines A set of line numbers that are clicked.
+ * @param decorationsCollection The decorations collection to update.
+ */
 
+editorReady
+    .then((editor) => {
+        const model = editor.getModel();
+        if (!model) {
+            console.error("No model is attached to the editor.");
+            return;
+        }
+
+        let lineCount = model.getLineCount();
+        const decorationsCollection = editor.createDecorationsCollection([]);
+        const clickedLines = new Set<number>();
+
+        // Initialize decorations
+        updateDecorations(editor, lineCount, clickedLines, decorationsCollection);
+
+        // Update decorations on content change
+        model.onDidChangeContent(() => {
+            const newLineCount = model.getLineCount();
+            if (newLineCount !== lineCount) {
+                lineCount = newLineCount;
+                updateDecorations(editor, lineCount, clickedLines, decorationsCollection);
+            }
+        });
+
+        // Event listen for decoration or line number click
+        editor.onMouseDown((event) => {
+            if (
+                event.target.type === monaco.editor.MouseTargetType.GUTTER_GLYPH_MARGIN ||
+                event.target.type === monaco.editor.MouseTargetType.GUTTER_LINE_NUMBERS
+            ) {
+                const lineNumber = event.target.position?.lineNumber;
+                if (lineNumber) {
+                    if (clickedLines.has(lineNumber)) {
+                        clickedLines.delete(lineNumber);
+                    } else {
+                        clickedLines.add(lineNumber);
+                    }
+                    updateDecorations(editor, lineCount, clickedLines, decorationsCollection);
+                }
+            }
+        });
+    })
+    
+    function updateDecorations(
+        editor: monaco.editor.IStandaloneCodeEditor,
+        lineCount: number,
+        clickedLines: Set<number>,
+        decorationsCollection: monaco.editor.IEditorDecorationsCollection
+    ) {
+        const decorations = [];
+        for (let i = 1; i <= lineCount; i++) {
+            decorations.push({
+                range: new monaco.Range(i, 1, i, 1),
+                options: {
+                    isWholeLine: false,
+                    glyphMarginClassName: clickedLines.has(i)
+                        ? 'red-circle-decoration'
+                        : 'red-circle-decoration opacity'
+                },
+            });
+        }
+    
+        decorationsCollection.set(decorations);
+    }
