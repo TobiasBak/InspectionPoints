1671a3175f019385e9e1a4541a8d0a65eb171965
TobiasBak <tobib21@student.sdu.dk>
2025-04-30 13:01:21 +0200

    Removed ReadVariableState Loop

--- python/WebsocketProxy.py
+++ python/WebsocketProxy.py
@@ -8,7 +8,6 @@ from typing import Final
 
 from websockets.server import serve
 
-
 from RobotControl.RunningWithSSH import run_script_on_robot
 from RobotControl.RobotSocketMessages import parse_robot_message, CommandFinished, ReportState, RobotSocketMessageTypes, \
     InterpreterCleared
@@ -18,7 +17,6 @@ from WebsocketNotifier import websocket_notifier
 from constants import ROBOT_FEEDBACK_PORT, FRONTEND_WEBSOCKET_PORT
 from custom_logging import LogConfig
 from undo.HistorySupport import handle_report_state, handle_command_finished, get_variable_registry
-from undo.ReadVariableState import report_state_received
 from RobotControl.Robot import Robot
 
 recurring_logger = LogConfig.get_recurring_logger(__name__)
@@ -30,9 +28,7 @@ _END_BYTE: Final = b'\x03'
 _EMPTY_BYTE: Final = b''
 
 _connected_web_clients = set()
-
 _new_client = False
-
 
 def handle_command_message(message: CommandMessage) -> str:
     command_string = message.data.command
@@ -131,7 +127,6 @@ async def open_robot_server():
     non_recurring_logger.info(f"ip_address of this container: {gethostbyname(gethostname())}")
     async with srv:
         non_recurring_logger.info('server listening for robot connections')
-        # await asyncio.create_task(start_read_loop()) COMMENTED BECAUSE IT BREAKS WHEN INTERPRETER IS NOT RUNNING
         await srv.serve_forever()
 
 
@@ -219,31 +214,11 @@ async def recover_mangled_data(extra_data: bytes) -> None:
     recurring_logger.debug(f"Fucked data: {extra_data}")
     # We have extra data and the rest was lost
     fucked_data = extra_data.decode()
-    # Regular expression pattern to extract "type", "id", and "data.command"
-    # if await search_for_command_finished(fucked_data):
-    #     return
 
     if await search_for_report_state(fucked_data):
         return
 
     recurring_logger.error("Pattern not found in the message.")
-
-
-# async def search_for_command_finished(fucked_data: str) -> bool:
-#     pattern = r'"type":\s*"([^"]+)".*?"id":\s*(\d+).*?"command":\s*"([^"]+)'
-#     # Use re.search to find the pattern in the string
-#     match = re.search(pattern, fucked_data)
-#     # If match is found, extract the values
-#     if match:
-#         message_type = match.group(1)
-#         id_value = int(match.group(2))
-#         command = match.group(3)
-#         match message_type:
-#             case RobotSocketMessageTypes.Command_finished.name:
-#                 send_command_finished(id_value, command)
-#                 recurring_logger.debug(f"Sending new Command finished with: id: '{id_value}' Command: '{command}'")
-#                 return True
-#     return False
 
 
 async def search_for_report_state(fucked_data: str) -> bool:
@@ -257,7 +232,7 @@ async def search_for_report_state(fucked_data: str) -> bool:
         match message_type:
             case RobotSocketMessageTypes.Report_state.name:
                 recurring_logger.warning("Found a match for report state in fucked data")
-                report_state_received()
+                # WE SHOULD PROLLY DO SOMETHING IF WE FIND A MATCH, BEFORE WE STARTED A NEW READ REPORT STATE
                 return True
     return False
 
@@ -285,11 +260,7 @@ def message_from_robot_received(message: bytes):
             send_to_all_web_clients(str(robot_message))
         case ReportState():
             handle_report_state(robot_message)
-            report_state_received()
             send_to_all_web_clients(str(robot_message))
-        # case InterpreterCleared():
-        #     handle_cleared_interpreter(robot_message)
-        #     report_state_received()
         case _:
             raise ValueError(f"Unknown RobotSocketMessage message: {robot_message}")
 
